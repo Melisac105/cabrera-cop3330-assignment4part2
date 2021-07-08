155cf425e043f608fdf108940c32083e6aebc87c
Melissa Cabrera <melisac105@gmail.com>
2021-07-07 21:54:52 -0400

    Update main window GUI

--- src/main/java/ucf/assignments/MainWindowControllers.java
+++ src/main/java/ucf/assignments/MainWindowControllers.java
@@ -7,7 +7,12 @@ package ucf.assignments;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
 public class MainWindowControllers {
